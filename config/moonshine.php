ad687146fe1d05bc39d3b1fa4cedc2c478f14822
halimdirgantara <57507252+halimdirgantara@users.noreply.github.com>
2023-05-03 14:13:42 +0700

    feat: org resource and config

--- config/moonshine.php
+++ config/moonshine.php
@@ -7,11 +7,11 @@ return [
     'dir' => 'app/MoonShine',
     'namespace' => 'App\MoonShine',
 
-    'title' => env('MOONSHINE_TITLE', 'MoonShine'),
+    'title' => env('MOONSHINE_TITLE', 'Admin Website'),
     'logo' => env('MOONSHINE_LOGO', ''),
 
     'route' => [
-        'prefix' => env('MOONSHINE_ROUTE_PREFIX', 'moonshine'),
+        'prefix' => env('MOONSHINE_ROUTE_PREFIX', 'admin'),
         'middleware' => ['web', 'moonshine'],
         'custom_page_slug' => 'custom_page',
         'notFoundHandler' => MoonShineNotFoundException::class
